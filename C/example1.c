20f4c0128e884d5c9d0ae3c2f11878a7f310397a
4n4l1st-ch4rl3s <63286909+4n4l1st-ch4rl3s@users.noreply.github.com>
2023-05-09 04:32:14 +0300

    more preprocessor

---
+++ C/example1.c
@@ -0,0 +1,12 @@
+#include "example1.h"
+#include <stdio.h>
+
+int main()
+{
+	int x = A;
+	float pie = PI;
+
+	printf("%d\n", x);
+	printf("The value of PI is: %.3f", pie);
+	return (0);
+}
---
+++ C/example1.h
@@ -0,0 +1,5 @@
+#ifndef EXAMPLE1
+#define A 10 // what ever is after define is called a macro
+#define PI 3.142
+
+#endif
